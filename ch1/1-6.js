e9b6746fef23f8c5ced89b0c929e4871af3fcbe6
Eugene Choe <eugene.choe@gmail.com>
2016-12-27 11:23:30 -0800

    Add remaining chapter 1 questions

---
+++ ch1/1-6.js
@@ -0,0 +1,2 @@
+// String Compression: Implement a method to perform basic string compression using the counts of repeated chars. For example, the string aabcccccaaa would be come a2b1c5a3. If the compressed str would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a-z)
+
---
+++ ch1/1-9.js
@@ -0,0 +1,1 @@
+// String Rotation: Assume you have a method isSubstring which checks if one word is a substirng of antoher. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g. 'waterbottle' is a rotation of 'erbottlewat').
