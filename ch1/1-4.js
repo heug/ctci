a0ec04c9dfbd3bf272a11a79656c86d91818450c
Eugene Choe <eugene.choe@gmail.com>
2016-12-27 11:36:03 -0800

    Complete prompt 1-4 Palindromes

--- ch1/1-4.js
+++ ch1/1-4.js
@@ -2,5 +2,34 @@
 
 var palPerm = function(str) {
   var word = str.split(' ').join('');
-  for (var i = 0; i < 
+  var store = {};
+  for (var i = 0; i < word.length; i++) {
+  	if (store[word[i]]) {
+  		store[word[i]]++;
+  	} else {
+  		store[word[i]] = 1;
+  	}
+  }
+
+  if (word.length % 2 === 0) {
+  	for (var key in store) {
+  		if (store[key] % 2 === 1) {
+  			return false;
+  		}
+  	}
+  } else {
+  	var oddValue = false;
+  	for (var key in store) {
+  		if (store[key] % 2 === 1) {
+  			if (oddValue) {
+  				return false;
+  			}
+  			oddValue = true;
+  		}
+  	}
+  }
+  return true;
 }
+
+
+console.log(palPerm('taco cat'));
