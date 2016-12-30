76b5c11aca5fd8113fd212a45d556639b6365187
Eugene Choe <eugene.choe@gmail.com>
2016-12-29 18:11:53 -0800

    CTCI 1-7 WIP

--- ch1/1-7.js
+++ ch1/1-7.js
@@ -1,1 +1,16 @@
 // Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+var rotateMatrix = function(arr) {
+
+	return arr;
+}
+
+console.log(rotateMatrix([[1,2,3],[4,5,6],[7,8,9]]))
+
+123
+456
+789
+
+741
+852
+963
